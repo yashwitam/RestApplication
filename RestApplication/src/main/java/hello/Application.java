af19dca418ae0118393e6b12db2b81ccafdd8840
yashwitam <ymishra>
2016-09-23 09:13:35 -0700

    java mail
    java mail

--- RestApplication/src/main/java/hello/Application.java
+++ RestApplication/src/main/java/hello/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	//Use the command to start application: java -jar target/RestApplication.jar --server.port=8181
+	//Access it on: http://localhost:8181/greeting
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
